4365484743a7fe877863f95488f17038d63950fb
Jesper Fråstad <jesper.frastad@gmail.com>
2022-12-05 23:07:11 +0100

    Day4 2022

---
+++ puzzles/2022/day4/part1.py
@@ -0,0 +1,13 @@
+import os
+
+with open(f'{os.path.dirname(__file__)}/input.txt', 'r') as f:
+    count = 0
+   
+    for line in f:
+        f = range(int(line.split('-')[0]), int(line.split('-')[1].split(',')[0])) 
+        s = range(int(line.split('-')[1].split(',')[1]), int(line.split('-')[2]))
+        fins = (s.start <= f.start <= s.stop) and (s.start <= f.stop <= s.stop)
+        sins = (f.start <= s.start <= f.stop) and (f.start <= s.stop <= f.stop)
+        count += 1 if fins or sins else 0
+  
+    print(count)
