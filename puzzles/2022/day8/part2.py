cedfae11fbe24314e7fd89252f9d9f19032f6527
Jesper Fråstad <jesper.frastad@gmail.com>
2022-12-08 22:55:46 +0100

    Made it a bit more efficient...

--- puzzles/2022/day8/part2.py
+++ puzzles/2022/day8/part2.py
@@ -10,19 +10,20 @@ def trees_in_line(proposal, line):
     return trees
 
 with open(f'{os.path.dirname(__file__)}/input.txt', 'r') as f:
-    grid = []
+    row_grid = []
     for line in f:
         line = line.strip()
-        grid.append(list(map(int, list(line)))) 
+        row_grid.append(list(map(int, list(line))))
 
+    col_grid = list(map(list, zip(*row_grid)))
     scenic_score = 0
-    for ri, r in enumerate(grid):
+    for ri, r in enumerate(row_grid):
         for ci, c in enumerate(r):
             view = 1
-            view *= trees_in_line(c, list(map(list, zip(*grid)))[ci][:ri][::-1])    #up
-            view *= trees_in_line(c, list(map(list, zip(*grid)))[ci][ri+1:])        #down
-            view *= trees_in_line(c, grid[ri][ci+1:])                               #right
-            view *= trees_in_line(c, grid[ri][:ci][::-1])                           #left
+            view *= trees_in_line(c, col_grid[ci][:ri][::-1])    #up
+            view *= trees_in_line(c, col_grid[ci][ri+1:])        #down
+            view *= trees_in_line(c, row_grid[ri][ci+1:])        #right
+            view *= trees_in_line(c, row_grid[ri][:ci][::-1])    #left
             scenic_score = view if view > scenic_score else scenic_score 
 
     print(scenic_score)
