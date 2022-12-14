a6910dc1f3f37cb6b8acc3c43072154b6c949acd
Jesper Fråstad <jesper.frastad@gmail.com>
2022-12-14 16:56:03 +0100

    Complete day14 2022

--- puzzles/2022/day14/part1.py
+++ puzzles/2022/day14/part1.py
@@ -1,36 +1,85 @@
 import os
-import turtle
+
+
+class Range:
+    def __init__(self, min, max):
+        self.min = min
+        self.max = max
+
+    def update(self, val):
+        self.min = min(val, self.min)
+        self.max = max(val, self.max)
+
+    def diff(self):
+        return self.max - self.min
+
+
+class Cord:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 
 with open(f'{os.path.dirname(__file__)}/input.txt', 'r') as f:
     scan = []
-    x_range = None
-    y_range = None
+    x_range = Range(500, 500)
+    y_range = Range(0, 0)
     paths = []
     for line in f:
         cords = line.strip().split(' -> ')
+        path = []
         for c in cords:
             x, y = map(int, c.split(','))
-            paths.append((x,y))
-            if not x_range:
-                x_range = (x,x)
-            elif x_range[0] > x:
-                x_range = (x, x_range[1])
-            elif x_range[1] < x:
-                x_range = (x_range[0], x)
-            if not y_range:
-                y_range = (y,y)
-            elif y_range[0] > y:
-                y_range = (y, y_range[1])
-            elif y_range[1] < y:
-                y_range = (y_range[0], y)
+            path.append(Cord(x, y))
+            x_range.update(x)
+            y_range.update(y)
+        paths.append(path)
 
-    for i in range(y_range[1]-y_range[0]):
-        print('.'*(x_range[1]-x_range[0]))
-    print(paths)
-    print(x_range)
-    print(y_range)
+    for i in range(y_range.diff()+1):
+        scan.append([])
+        for j in range(x_range.diff()+1):
+            scan[i].append('.')
 
-    wn = turtle.Screen()
-    wn.bgcolor("light green")
-    wn.title("Turtle")
-    skk = turtle.Turtle()
+    for path in paths:
+        for i in range(len(path)-1):
+            x1 = path[i].x - x_range.min
+            x2 = path[i+1].x - x_range.min
+            y1 = path[i].y - y_range.min
+            y2 = path[i+1].y - y_range.min
+
+            # x
+            if x1 == x2:
+                for j in range(min(y1, y2), max(y1, y2)):
+                    scan[j][x1] = '#'
+            # y
+            if y1 == y2:
+                for j in range(min(x1, x2), max(x1, x2)+1):
+                    scan[y1][j] = '#'
+
+    # start dropping sand
+    s = Cord(500-x_range.min, 0-y_range.min)
+    sand_units = 0
+    while True:
+        if s.y == y_range.max:
+            print(f'Falling of the edge at {s.x}:{s.y}')
+            break
+        # d
+        elif scan[s.y+1][s.x] == '.':
+            s.y += 1
+        # ld
+        elif s.x != x_range.min and scan[s.y+1][s.x-1] == '.':
+            s.y += 1
+            s.x += -1
+        # rd
+        elif s.x != x_range.max and scan[s.y+1][s.x+1] == '.':
+            s.y += 1
+            s.x += 1
+        # settle
+        else:
+            scan[s.y][s.x] = 's'
+            s = Cord(500-x_range.min, 0-y_range.min)
+            sand_units += 1
+
+    for r in scan:
+        print(''.join(r))
+    print(sand_units)
--- puzzles/2022/day14/part2.py
+++ puzzles/2022/day14/part2.py
@@ -1,43 +1,94 @@
 import os
-import ast
 
-def comp(p1, p2):
-    if isinstance(p1, list) and isinstance(p2, list):
-        for pp1, pp2 in zip(p1,p2):
-            ret = comp(pp1, pp2)
-            if ret is not None:
-                return ret
-        return comp(len(p1), len(p2))
-    elif isinstance(p1, list):
-        return comp(p1, [p2])
-    elif isinstance(p2, list):
-        return comp([p1], p2)
-    elif p1 < p2:
-        return True
-    elif p1 > p2:
-        return False
-    else:
-        return None
+
+class Range:
+    def __init__(self, min, max):
+        self.min = min
+        self.max = max
+
+    def update(self, val):
+        self.min = min(val, self.min)
+        self.max = max(val, self.max)
+
+    def diff(self):
+        return self.max - self.min
+
+
+class Cord:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
 with open(f'{os.path.dirname(__file__)}/input.txt', 'r') as f:
-    pairs = []
+    sand_start = Cord(500, 0)
+    scan = []
+    x_range = Range(sand_start.x, sand_start.x)
+    y_range = Range(sand_start.y, sand_start.y)
+    paths = []
     for line in f:
-        if line != '\n':
-            pairs.append(ast.literal_eval(line))
+        cords = line.strip().split(' -> ')
+        path = []
+        for c in cords:
+            x, y = map(int, c.split(','))
+            path.append(Cord(x, y))
+            x_range.update(x)
+            y_range.update(y)
+        paths.append(path)
 
-    pos2 = 1
-    pos6 = 2
-    for i in range(int((len(pairs))/2)):
-        p1 = pairs[i*2]
-        p2 = pairs[i*2+1]
-        if comp(p1, [[2]]):
-            pos2 += 1
-        if comp(p2, [[2]]):
-            pos2 += 1
-        if comp(p1, [[6]]):
-            pos6 += 1
-        if comp(p2, [[6]]):
-            pos6 += 1
+    y_range.max += 2
+    x_range.min = sand_start.x-500
+    x_range.max = sand_start.x+500
 
-    print(pos2*pos6)
+    # normalize into our cordinate system
+    sand_start.x -= x_range.min
+    sand_start.y -= y_range.min
+
+    paths.append([Cord(x_range.min, y_range.max),
+                 Cord(x_range.max, y_range.max)])
+    for i in range(y_range.diff()+1):
+        scan.append([])
+        for j in range(x_range.diff()+1):
+            scan[i].append('.')
+
+    for path in paths:
+        for i in range(len(path)-1):
+            # normalize
+            x1 = path[i].x - x_range.min
+            x2 = path[i+1].x - x_range.min
+            y1 = path[i].y - y_range.min
+            y2 = path[i+1].y - y_range.min
+
+            # x
+            if x1 == x2:
+                for j in range(min(y1, y2), max(y1, y2)):
+                    scan[j][x1] = '#'
+            # y
+            if y1 == y2:
+                for j in range(min(x1, x2), max(x1, x2)+1):
+                    scan[y1][j] = '#'
+
+    # start dropping sand
+    s = Cord(sand_start.x, sand_start.y)
+    sand_units = 0
+    while True:
+        if s.x == sand_start.x and s.y == sand_start.y and scan[s.y+1][s.x-1] == 's' and scan[s.y+1][s.x] == 's' and scan[s.y+1][s.x+1] == 's':
+            sand_units += 1
+            print(f'Topped up after {sand_units} units of sand has fallen')
+            break
+        # d
+        elif scan[s.y+1][s.x] == '.':
+            s.y += 1
+        # ld
+        elif s.x != x_range.min and scan[s.y+1][s.x-1] == '.':
+            s.y += 1
+            s.x += -1
+        # rd
+        elif s.x != x_range.max and scan[s.y+1][s.x+1] == '.':
+            s.y += 1
+            s.x += 1
+        # settle
+        else:
+            scan[s.y][s.x] = 's'
+            s = Cord(sand_start.x, sand_start.y)
+            sand_units += 1
